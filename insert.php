65d39a8c221009a7c97124066bbbddfdaa1e017f
Guillermo-alt <guillermo76ov@hotmail.com>
2021-03-10 23:22:01 -0600

    1er conexion php xamp

---
+++ insert.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$database = "prueba";
+$username = "root";
+$password = "Kinomoto76";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+}
+ 
+echo "Connected successfully";
+
+$sql = "INSERT INTO Comentarios (Nombre, Correo, Asunto) VALUES ('Test', 'Testing', 'Testing@tesing.com')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
