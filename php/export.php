d78426b2ffcb5615e188b96cf1ba1553c0afeded
Guillermo-alt <guillermo76ov@hotmail.com>
2021-04-27 04:59:02 -0500

    script de casos para subir lento, demo registro comentarios y excel

--- php/export.php
+++ php/export.php
@@ -1,11 +1,10 @@
 <?php
-require "conect.php";
+require "connect.php";
 // Check connection
 if (!$conn) {
       die("Connection failed: " . mysqli_connect_error());
 }
- 
-echo "Connected successfully";
+
 
 $sql = "SELECT * FROM comentarios";
 $result = $conn->query($sql);
@@ -13,12 +12,31 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+   $records_group[] = $row;
   }
 } else {
   echo "0 results";
 }
-mysqli_close($conn);
+   $timestamp =date('d-m-Y');
+            $filename = 'coment_' . $timestamp . '.xls';
+            
+            header("Content-type: application/x-msdownload");
+            header("Content-Disposition: attachment; filename=\"$filename\"");
+            
 
+            
+            $isPrintHeader = false;
+
+            foreach ($records_group as $row_c) {
+
+                if (! $isPrintHeader ) {
+
+                    echo implode("\t", array_keys($row_c)) . "\n";
+                    $isPrintHeader = true;
+
+                }
+                echo implode("\t", array_values($row_c)) . "\n";
+            }
+            exit();
 mysqli_close($conn);
 ?>
--- php/insert.php
+++ php/insert.php
@@ -1,24 +1,24 @@
 <?php
-require "conect.php";
+require "connect.php";
 // Check connection
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-}
- 
-echo "Connected successfully";
 
-$matric =$_POST['matricula'];
+$matri = $_POST['matricula'];
 $email = $_POST['email'];
 $asunto = $_POST['asunto'];
 
-$sql = "INSERT INTO comentarios (Matricula,Email,Asunto) VALUES ('$matric', '$email', '$asunto')";
-if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
+echo $matri . " ".$asunto;
+$sql = "INSERT INTO comentarios (Matricula, Email, Asunto) VALUES ('$matri','$email','$asunto')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
 } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  echo "Error: " . $sql . "<br>" . $conn->error;
 }
-mysqli_close($conn);
+
+$conn->close();
 ?>
+
 <script> alert("Mensaje Registrado");
-location.href ="/contacto.html";
+location.href ="/WEB-LENS/contacto.html";
+//location.href ="/contacto.html";
 </script>
