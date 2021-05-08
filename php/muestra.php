63578b50185083df6ed1b6ee9b5b61e7afa785f9
Guillermo-alt <guillermo76ov@hotmail.com>
2021-05-08 16:17:35 -0500

    se agrego el archivo muestra.php

---
+++ php/muestra.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Guillermo Ortega Vargas" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Enlace a la biblioteca de bootstrap 5- archivos css-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="/my_css/style">
+    <link rel="shortcut icon" href="img/favicon1.png"/>
+    <title>Investigación</title>
+</head>
+<body class="container-fluid"> 
+
+<div class="row">
+    <header class="col-md-12 "><a href="index.html"><img src="img/header.png" alt="logotivo Lens"></a></header>
+    <nav class="navbar navbar-expand-md navbar-dark nav-pill ">
+      <div class="container-fluid  ">
+        <a class="navbar-brand  " href="#"></a><!--sobra pero se necesita para espacio en icono nav-->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon "></span><!--icono de nav-->
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav ">
+            <li class="nav-item ">
+              <a class="nav-link"  href="index.html">HOME</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link"  href="acerca.html">ACERCA</a>
+            </li>
+            <li class="nav-item ">
+              <a class="nav-link"  href="diseno.html">DISEÑO</a><!--INFORMACION -METODOS --- PARA dISEÑO-->
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="recursos.html" id="navbarDropdown" role="button">
+                RECURSOS
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">HERRAMIENTAS</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="investigacion.html">INVESTIGACIÓN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contacto.html">CONTACTO</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+      </div>
+      <div class="row">
+        <div class="col-md-12 container px-5 pt-4 contenido1 cuerpo-1">
+          <h2 class="text-center Titulo-1">Investigación</h2>
+          <p>Lorem ipsum dolor sit amet Velit rerum dolore, sed tempora esse amet aperiam a cumque culpa assumenda laboriosam nisi placeat deleniti, numquam voluptates accusamus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla neque aliquid ipsam numquam distinctio error, magni, praesentium voluptatum quos, assumenda expedita eos para tenmer una recusandae. Adipisci alias expedita fuga.</p>
+        </div>  
+      </div>
+
+      <!--popper para ventas y botones emergentes, bootstrap.js-->
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>
